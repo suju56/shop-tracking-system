f430d3fdde4cc926de335dc7815766744f294b79
sujana00 <sujanaamgain11@gmail.com>
2022-11-19 08:17:11 +0545

    map update

--- src/Pages/Shop/Shoppage.js
+++ src/Pages/Shop/Shoppage.js
@@ -102,16 +102,3 @@ export const Shoppage = () => {
   );
 };
 
-render(
-  <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-);
